050d502d48e4fbb1f9ef5e50d4b80d5a3b890f66
LepkoQQ <lepko.san@gmail.com>
2020-02-05 14:01:57 +0100

    add vue-i18n

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/gp-lottery/' : '/',
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
 };
